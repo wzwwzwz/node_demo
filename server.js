9e82ea4c253777d85c1279b3602b013bb6cdb18b
wzw <15578464617@qq.com>
2020-09-12 09:39:33 +0800

    添加过滤非POST请求，即是axios请求会发两个请求；且在该项目下已确定全都是用post请求；

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var http = require("http");
 var url = require("url");
 
+var dataHandle = require("./common/dataHandle");
 
 //获取到单一参数要用到的模块
 // var queryString = require("querystring");
 // var util = require("util");
 
-var dataHandle = require("./common/dataHandle");
+var mysql = require("mysql");
+
+require("./common/Buffer");
 
 function start(route, handle) {
     function onRequest(request, response) {
@@ -19,6 +22,18 @@ function start(route, handle) {
 
         if ("OPTIONS" === request.method) {
             // return;
+        }
+
+        // resolve axios send two request; one is "OPTION"; another is real request
+        if ("POST" !== request.method ) {
+            response.writeHead(200, {
+                "Content-Type": "text/plain;charset=utf-8",
+                // "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "*",
+            });
+            response.end();
+            return;
         }
 
         // timeout
@@ -52,10 +67,10 @@ function start(route, handle) {
                 return;
             }
 
-            // ungzip && 解压
+            // ungzip && 解密
             postData = dataHandle.decrypt(dataHandle.ungzip(postData));
             console.log("[路径名] -- " + pathname);
-            console.log("[REQUEST PARAM UNGZIP] -- ",postData);
+            // console.log("[REQUEST PARAM UNGZIP] -- ",postData);
 
             // get response data
             var returnVal = route(handle, pathname, JSON.parse(postData));
@@ -71,8 +86,7 @@ function start(route, handle) {
             // 加密 && 压缩
             let param = dataHandle.gzip(dataHandle.encrypt(JSON.stringify(returnVal)));
 
-            console.log("[RESPONSE PARAM] -- ",param);
-
+            // console.log("[RESPONSE PARAM] -- ",param);
             // console.log("再来一遍",dataHandle.decrypt(dataHandle.ungzip(param)));
 
             response.end(param);
@@ -80,14 +94,44 @@ function start(route, handle) {
     }
 
     var server = http.createServer(onRequest);
+    var post = 3000;
 
-    server.listen(3000);
-
-    console.log("Server running at http://localhost:3000/");
+    server.listen(post);
+    console.log(`Server running at http://localhost:${post}/`);
 
 
     // 压缩文件
     dataHandle.gzipFile("./test/1.txt");
+
+
+    // test_connect_sql();
+    function test_connect_sql() {
+        var connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password: "123456",
+            port: "3306",
+            database: "test"
+        });
+
+        connection.connect();
+
+        var sql = "SELECT * FROM websites";
+        //查
+
+        connection.query(sql,function (err, result) {
+            if (err) {
+                console.log("[SELECT ERROR] - ",err.message);
+                return;
+            }
+
+            console.log("--------------------------SELECT----------------------------");
+            console.log(result);
+            console.log("------------------------------------------------------------\n\n");
+        });
+
+        connection.end();
+    }
 }
 
 exports.start = start;
